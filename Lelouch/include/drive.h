87dac2d6043be6c29de6b24691b2db86b774996d
rubin230 <ruhintauro1@gmail.com>
2026-02-17 18:58:18 -0800

    Add files via upload

---
+++ Lelouch/include/drive.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "lemlib/api.hpp"
+#include "pros/misc.h"
+#include "pros/motors.hpp"
+#include "pros/adi.hpp"
+#include "pros/imu.hpp"
+#include "pros/distance.hpp"
+#include "pros/rtos.hpp"
+#include "robot-config.h"
+
+extern void debug_screen();
